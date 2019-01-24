107b19a2eb6a5d26403dcc734db0521a0a99faf8
Scott Vojik <scott.vojik@gmail.com>
2019-01-23 16:22:10 -0800

    MVP finished, sorry marious I forgot to commit more. Working on it I promise

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,25 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import axios from "axios";
 import './App.css';
 
 class App extends Component {
+  state = {
+    friends: [],
+    error: ""
+  };
+
+  componentDidMount() {
+    axios.get("http://localhost:5000/friends")
+    .then(res => {
+      console.log(res);
+      this.setState({ friends: res.data });
+    })
+    .catch(err => {
+      this.setState({ error: err.response.data.message });
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Lambda Friends!</h1>
+        {this.state.error && <h3>{this.state.error}</h3>}
+
+        {this.state.friends.map(friend => (
+          <div>
+          <h2>{friend.name}</h2>
+          <p>{friend.age}</p>
+          <p>{friend.email}</p>
+          </div>
+        ))}
       </div>
     );
   }
